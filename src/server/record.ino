c140a24bacd6e2695bbe20240225720f8f1d1057
Remy <107559570+hrszpuk@users.noreply.github.com>
2023-06-28 21:28:35 +0100

    Added record class

---
+++ src/server/record.ino
@@ -0,0 +1,26 @@
+class Record {
+  char** instructions;
+  int count;
+
+public:
+  Record() {
+    count = 0;
+
+  }
+
+  ~Record() {
+
+  }
+
+  void play() {
+
+  }
+
+  void push() {
+    
+  }
+
+  void pop() {
+
+  }
+};
