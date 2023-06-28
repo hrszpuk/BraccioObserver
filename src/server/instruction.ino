25931933f1b9520eb302ab824a15f6993253d8c1
Remy <107559570+hrszpuk@users.noreply.github.com>
2023-06-28 21:28:21 +0100

    Added instruction class

---
+++ src/server/instruction.ino
@@ -0,0 +1,21 @@
+class Instruction {
+  char kind;
+  int* motors;
+  int* degrees;
+  int length;
+  int id;
+
+public:
+  Instruction(char** data) {
+    
+  }
+
+  ~Instruction() {
+
+  }
+
+  void execute() {
+
+  }
+};
+
